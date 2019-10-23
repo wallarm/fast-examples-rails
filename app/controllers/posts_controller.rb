88a21797821c886b1106f96cba60ed8417e05add
Yaroslav Sorokin <yasorokin@wallarm.com>
2019-10-23 15:32:01 +0300

    Update descriptions and change default wallarm_api_host

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,19 +26,20 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    # @post = Post.new(post_params)
+    @post = Post.new(post_params)
 
-    # respond_to do |format|
-    #   if @post.save
-    #     format.html { redirect_to @post, notice: 'Post was successfully created.' }
-    #     format.json { render :show, status: :created, location: @post }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @post.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
 
-    render html: post_params.to_s.html_safe
+    # NOTE: We can add vuln to fail build. Also comment all previous code in this method.
+    # render html: post_params.to_s.html_safe
   end
 
   # PATCH/PUT /posts/1
